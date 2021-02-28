7cc3289689c55f54ecc2595ad8aa27c492e6031c
Nick Gorjestani <nickg2794@hotmail.com>
2021-02-27 21:09:33 -0600

    Initial commit

---
+++ src/main/java/edu/wctc/DriversLicense.java
@@ -0,0 +1,10 @@
+package edu.wctc;
+
+public class DriversLicense {
+    private String SSSS;
+    private int FFF;
+    private int YY;
+    private int DDD;
+    private int NN;
+
+}
---
+++ src/main/java/edu/wctc/WisconsinFormatter.java
@@ -0,0 +1,4 @@
+package edu.wctc;
+
+public class WisconsinFormatter implements DriversLicenseFormatter{
+}
